1a8c4fb7c52d8b9781876a04cec030fca59d6edb
OlexandrPl <sutjagin@gmail.com>
2022-05-07 22:15:46 +0300

    Implemented methods of List class.

--- src/org/way2it/data_structures/list/List.java
+++ src/org/way2it/data_structures/list/List.java
@@ -9,35 +9,86 @@ public class List {
     private int length = 0;
 
     public List() {
+
     }
 
     // Should add new value to the end of the list, increment length and extend array by x2 if needed
     public void add(String value) {
-        // TODO implement me
+
+        if(values.length <= length) {
+            String[] buffer = values.clone();
+            values = new String[length * 2];
+            System.arraycopy(buffer, 0, values, 0, buffer.length);
+        }
+        values[length] = value;
+        length++;
+
     }
 
     // Should add new value at the specified index, moving other values to the right,
     // increment length and extend array by x2 if needed
     public void addAtIndex(String value, int index) {
-        // TODO implement me
+
+        if(values.length <= length) {
+            String[] buffer = values.clone();
+            values = new String[length * 2];
+            System.arraycopy(buffer, 0, values, 0, buffer.length);
+        }
+        int j = 0;
+        String[] tmpList = new String[values.length];
+        for(int i = 0; i < tmpList.length; i++) {
+            if(i == index) {
+                tmpList[i] = value;
+            } else {
+                tmpList[i] = values[j];
+                j++;
+            }
+
+        }
+        values = tmpList.clone();
+
+        length++;
     }
 
     // Should return a value at specified index, or null if index is out of list bounds
     public String get(int index) {
-        // TODO implement me
-        return null;
+
+        if(index > length) {
+            return null;
+        } else {
+            return values[index];
+        }
+
     }
 
     // Should return the index of the first occurrence of specified value in list
     // If the value does not exist - return -1
     public int indexOf(String value) {
-        // TODO implement me
+
+        for(int i = 0; i < values.length; i++) {
+            if(values[i] != null) {
+                if(values[i].equals(value)) {
+                    return i;
+                }
+            }
+        }
+
         return -1;
     }
 
     // Should remove the value at specified index, decrementing the length of this list and moving next values to the left
     public void remove(int index) {
-        // TODO implement me
+
+        int j = 0;
+        String[] tmpList = new String[values.length-1];
+        for(int i = 0; i < values.length; i++)
+            if(i != index) {
+                tmpList[j] = values[i];
+                j++;
+            }
+        values = tmpList.clone();
+
+        length--;
     }
 
     public int getLength() {
