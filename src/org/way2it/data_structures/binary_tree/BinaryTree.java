ba9df10dbaf5b8255e5663c6e7825528643782a9
OlexandrPl <sutjagin@gmail.com>
2022-05-16 20:48:46 +0300

    Binary processs

--- src/org/way2it/data_structures/binary_tree/BinaryTree.java
+++ src/org/way2it/data_structures/binary_tree/BinaryTree.java
@@ -1,5 +1,7 @@
 package org.way2it.data_structures.binary_tree;
 
+import org.way2it.data_structures.linked_list.LinkedList;
+
 public class BinaryTree {
 
     // Represents the first node of this tree
@@ -9,27 +11,118 @@ public class BinaryTree {
     private int size = 0;
 
     public BinaryTree() {
+
     }
 
     // Should add new value to the tree according to binary tree rules:
     // Values that are less than or equal to the value in the current node, should be placed in the left subtree
     // Values that are greater than the value in the current node - should be placed in the right subtree
     public void add(int value) {
-        // TODO implement me
+        Node newNode = new Node();
+        Node currNode = root;
+        if (root==null){
+           root=newNode;
+           root.value=value;
+           size++;
+        }else {
+            addTree(value,root);
+
+        }
     }
+    private void addTree(int tValue, Node currRoot){
+
+        if(currRoot.value >tValue) {
+            if(currRoot.left==null){
+                Node newNod = new Node();
+                newNod.value=tValue;
+                currRoot.left=newNod;
+                size++;
+            }else{
+                addTree(tValue, currRoot.left);
+            }
+        }else{
+            if(currRoot.right==null){
+                Node newNod = new Node();
+                newNod.value=tValue;
+                currRoot.right=newNod;
+                size++;
+            }else{
+                addTree(tValue,currRoot.right);
+            }
+
+        }
+
+    }
+
 
     // Should remove specified value from tree and return true
     // If value does not exist in this tree - return false
     public boolean remove(int value) {
-        // TODO implement me
-        return false;
+        if (contains(value)){
+
+            return removeTree(value, root, root);
+        }
+       else{
+            return false;
+        }
+
+    }
+
+    private boolean removeTree(int tValue, Node currRoot, Node preRoot){
+
+        if (currRoot.value==tValue){
+            preRoot.right=currRoot.right;
+            preRoot.left=currRoot.left;
+            size--;
+            return true;
+        }else {
+
+            if(currRoot.value >tValue) {
+                if(currRoot.left==null){
+                    return false;
+                }else{
+                    return     removeTree(tValue, currRoot.left, currRoot);
+                }
+            }else{
+                if(currRoot.right==null){
+                    return  false;
+                }else{
+                    return   removeTree(tValue,currRoot.right, currRoot);
+                }
+
+            }
+        }
+
     }
 
     // Should return true if this tree contains specified value, false - otherwise
     public boolean contains(int value) {
-        // TODO implement me
-        return false;
-    }
+        return contTree(value,root);
+
+       }
+     private boolean contTree(int tValue, Node currRoot) {
+
+        if (currRoot.value==tValue){
+            return true;
+        }else {
+            if(currRoot.value >tValue) {
+                if(currRoot.left==null){
+                    return false;
+                }else{
+                    return     contTree(tValue, currRoot.left);
+                }
+            }else{
+                if(currRoot.right==null){
+                    return  false;
+                }else{
+                    return   contTree(tValue,currRoot.right);
+                }
+
+            }
+        }
+
+
+     }
 
     public int getSize() {
         return size;
